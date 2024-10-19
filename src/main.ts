bdbce52b16d5d8e4666518c243e843a032db29fe
Linc <762699299@qq.com>
2024-10-19 18:30:32 +0800

    refactor: optimized writing

--- src/main.ts
+++ src/main.ts
@@ -1,74 +1,40 @@
-import {
-    App,
-    Modal,
-    Notice,
-    Plugin,
-    PluginSettingTab,
-    Setting
-} from 'obsidian';
-import type {MarkdownPostProcessorContext} from "obsidian"
+import { Plugin } from "obsidian";
+import type { MarkdownPostProcessorContext } from "obsidian"
 import { factoryVueDom } from './vueAdapt'
-import { MyView, VIEW_TYPE } from './view'
-
 
 interface MyPluginSettings {
-    mySetting: string;
+  mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-    mySetting: 'default'
+  mySetting: 'default'
 }
 
 export default class MyPlugin extends Plugin {
-    settings: MyPluginSettings;
+  settings: MyPluginSettings;
 
-    async onload() {
-        await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
-        this.registerView(
-            VIEW_TYPE,
-            (leaf) => new MyView(leaf)
-        )
+    this.registerMarkdownCodeBlockProcessor("vue-test",
+      (
+        src: string,                                // 代码块内容
+        blockEl: HTMLElement,                       // 代码块渲染的元素
+        ctx: MarkdownPostProcessorContext           // 上下文
+      ) => {
+        const root_div = document.createElement("div");  blockEl.appendChild(root_div); root_div.classList.add("vue-shell");
+        factoryVueDom(root_div, "vue-test")
+      }
+    )
+  }
 
-        this.addRibbonIcon('dice', 'Open my view', (evt) => {
-            this.activateView()
-        })
+  onunload() {}
 
-        this.registerMarkdownCodeBlockProcessor("vue-test",
-          (
-            src: string,                                // 代码块内容
-            blockEl: HTMLElement,                       // 代码块渲染的元素
-            ctx: MarkdownPostProcessorContext           // 上下文
-          ) => {
-            const root_div = document.createElement("div");  blockEl.appendChild(root_div); root_div.classList.add("vue-shell");
-            factoryVueDom(root_div, "vue-test")
-          }
-        )
-    }
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-    onunload() {
-        this.app.workspace.detachLeavesOfType(VIEW_TYPE)
-    }
-
-    async loadSettings() {
-        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-    }
-
-    async saveSettings() {
-        await this.saveData(this.settings);
-    }
-    async activateView() {
-        if (this.app.workspace.getLeavesOfType(VIEW_TYPE).length === 0) {
-            await this.app.workspace.getRightLeaf(false).setViewState({
-                type: VIEW_TYPE,
-                active: true,
-            })
-        }
-
-        this.app.workspace.revealLeaf(
-            this.app.workspace.getLeavesOfType(VIEW_TYPE)[0]
-        )
-    }
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
-
-
