22c2a9c2ad9eac8e0ce1abfb0b4484358eb0e28b
Linc <762699299@qq.com>
2024-10-19 18:03:03 +0800

    build: update package version

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <template>
     <h2>Hello,Developer!</h2>
-    <Hello></Hello>
     <Hi :hi="hi"></Hi>
-    <HiHi></HiHi>
 </template>
 
 <script setup lang="tsx">
-import Hello from "./Hello";
 import Hi from "./Hi.vue";
 import { ref } from "vue";
 let hi = ref("");
-let HiHi = () => (<h1><Hello></Hello></h1>)
-
 </script>
-
 
 <style scoped>
 h2 {
