f354f568ee9ca00f088fbc5d76a1a041ae271e5f
Linc <762699299@qq.com>
2025-01-31 18:48:00 +0800

    enhance: controls panel feat and style

--- src/NodeFlow/component/utils/InteractionControls.vue
+++ src/NodeFlow/component/utils/InteractionControls.vue
@@ -3,49 +3,49 @@
 <template>
   <Panel position="top-right">
     <div class="fold-title">
-      <button id="zoomin" @click="isFold=!isFold">Fold</button>
+      <button @click="isFold1=!isFold1">Canvas Config {{isFold1?"v":"^"}}</button>
     </div>
-    <div v-show="!isFold" class="fold-content">
+    <div v-show="!isFold1" class="fold-content">
       <div>
-        <label class="label" for="draggable">
+        <label for="draggable" title="允许拖拽节点">
           Nodes draggable
           <input id="draggable" v-model="nodesDraggable" type="checkbox" class="vue-flow__draggable" />
         </label>
       </div>
       <div>
-        <label class="label" for="connectable">
+        <label for="connectable" title="允许连接节点">
           Nodes connectable
-          <input id="connectable" v-model="nodesConnectable" type="checkbox" class="vue-flow__connectable" />
+          <input id="connectable" v-model="nodesConnectable" type="checkbox"/>
         </label>
       </div>
       <div>
-        <label class="label" for="selectable">
+        <label for="selectable" title="允许选择元素">
           Elements selectable
-          <input id="selectable" v-model="elementsSelectable" type="checkbox" class="vue-flow__selectable" />
+          <input id="selectable" v-model="elementsSelectable" type="checkbox"/>
         </label>
       </div>
       <div>
-        <label class="label" for="zoomonscroll">
+        <label for="zoomonscroll" title="滚轮时,进行缩放">
           Zoom on scroll
-          <input id="zoomonscroll" v-model="zoomOnScroll" type="checkbox" class="vue-flow__zoomonscroll" />
+          <input id="zoomonscroll" v-model="zoomOnScroll" type="checkbox"/>
         </label>
       </div>
       <div>
-        <label class="label" for="zoomonpinch">
+        <label for="zoomonpinch" title="Ctrl滚动时,进行缩放">
           Zoom on pinch
-          <input id="zoomonpinch" v-model="zoomOnPinch" type="checkbox" class="vue-flow__zoomonpinch" />
+          <input id="zoomonpinch" v-model="zoomOnPinch" type="checkbox"/>
         </label>
       </div>
       <div>
-        <label class="label" for="panonscroll">
+        <label for="panonscroll" title="滚轮时,进行滚动">
           Pan on scroll
-          <input id="panonscroll" v-model="panOnScroll" type="checkbox" class="vue-flow__panonscroll" />
+          <input id="panonscroll" v-model="panOnScroll" type="checkbox"/>
         </label>
       </div>
       <div>
-        <label class="label">
+        <label title="滚轮时,进行滚动的方向">
           Pan on scroll mode
-          <select id="panonscrollmode" v-model="panOnScrollMode" class="vue-flow__panonscrollmode">
+          <select v-model="panOnScrollMode">
             <option value="free">Free</option>
             <option value="horizontal">Horizontal</option>
             <option value="vertical">Vertical</option>
@@ -53,40 +53,47 @@
         </label>
       </div>
       <div>
-        <label class="label" for="zoomondbl">
+        <label for="zoomondbl" title="双击时进行放大">
           Zoom on double click
-          <input id="zoomondbl" v-model="zoomOnDoubleClick" type="checkbox" class="vue-flow__zoomondbl" />
+          <input id="zoomondbl" v-model="zoomOnDoubleClick" type="checkbox"/>
         </label>
       </div>
       <div>
-        <label class="label" for="panemoveable">
+        <label for="panemoveable" title="允许拖拽画布">
           Pane movable
-          <input id="panemoveable" v-model="panOnDrag" type="checkbox" class="vue-flow__panemoveable" />
+          <input id="panemoveable" v-model="panOnDrag" type="checkbox"/>
         </label>
       </div>
       <div>
-        <label class="label" for="capturezoompaneclick">
+        <label for="capturezoompaneclick" title="在窗格上捕获?">
           Capture on pane click
-          <input id="capturezoompaneclick" v-model="captureZoomClick" type="checkbox" class="vue-flow__capturezoompaneclick" />
+          <input id="capturezoompaneclick" v-model="captureZoomClick" type="checkbox"/>
         </label>
       </div>
       <div>
-        <label class="label" for="capturezoompanescroll">
+        <label for="capturezoompanescroll" title="在窗格上捕获?">
           Capture on pane scroll
-          <input id="capturezoompanescroll" v-model="captureZoomScroll" type="checkbox" class="vue-flow__capturezoompanescroll" />
+          <input id="capturezoompanescroll" v-model="captureZoomScroll" type="checkbox"/>
         </label>
       </div>
       <div>
-        <label class="label">
-          Zoom in
-          <button id="zoomin" :onclick="onZoomIn" class="vue-flow__capturezoompanescroll">+</button>
+        <label title="放大">
+          
+          <button :onclick="onZoomIn">Zoom in</button>
         </label>
       </div>
       <div>
-        <label class="label">
-          Zoom out
-          <button id="zoomout" :onclick="onZoomOut" class="vue-flow__capturezoompanescroll">-</button>
+        <label>
+          <button :onclick="onZoomOut">Zoom out</button>
         </label>
+      </div>
+    </div>
+    <div class="fold-title">
+      <button @click="isFold2=!isFold2">Nodes Tool {{isFold2?"v":"^"}}</button>
+    </div>
+    <div v-show="!isFold2" class="fold-content">
+      <div>
+        <button>Copy&Paste</button>
       </div>
     </div>
   </Panel>
@@ -97,6 +104,8 @@
 import { Panel, useVueFlow } from '@vue-flow/core'
 import { ref } from 'vue'
 import { nfSetting } from '../../utils/main/setting'
+let isFold1 = ref(true);
+let isFold2 = ref(true);
 
 const {
   nodesDraggable,
@@ -123,10 +132,10 @@ const {
 } = useVueFlow()
 
 // 全局设置
-let isFold = ref(true);
 const captureZoomClick = ref(false)
 const captureZoomScroll = ref(false)
 onConnect((params) => addEdges(params)) // nodesConnectable只是是否允许拖拽线出来，加这行后两个端点之间的线在鼠标松开后才能保持
+// zoomOnScroll.value = true;
 const onZoomIn = ()=>{ setMaxZoom(3); zoomIn(); };
 const onZoomOut = ()=>{ setMinZoom(0.1); zoomOut(); };
 onZoomIn(); onZoomOut(); // 应用缩放限制，set(Max/Min)Zoom后要zoom(In/Out)才能生效
@@ -142,35 +151,58 @@ onMoveEnd((flowTransform) => nfSetting.isDebug && console.log('Move end', flowTr
 
 <style scoped>
 .vue-flow__panel {
-    background-color:#2d3748;
-    padding:10px;
-    border-radius:8px;
-    box-shadow:0 0 10px #00000080;
-    color:#fff;
-    display:flex;
-    flex-direction:column;
-    gap:2px;
-    font-size:12px;
-    font-weight:600
-}
+  display:flex;
+  flex-direction:column;
+  gap:2px;
+  font-size:12px;
+  font-weight:600;
 
-.vue-flow__panel .label {
-    display:flex;
-    justify-content:space-between;
-    align-items:center;
-    gap:5px;
-    cursor:pointer
+  overflow-y: auto;
 }
 
 .vue-flow__panel .fold-title {
   width: 100%;
-  margin-bottom: 10px;
+  height: 24px;
+  margin: 2px 0;
 }
 .vue-flow__panel .fold-title button {
   width: 100%;
+  height: 100%;
 }
 
-.fold-content>div {
+.vue-flow__panel .fold-content {
+  background-color:#2d3748;
+  padding:10px;
+  border-radius:8px;
+  box-shadow:0 0 10px #00000080;
+  color:#fff;
+  display:flex;
+  flex-direction:column;
+  gap:2px;
+  font-size:12px;
+  font-weight:600;
+}
+
+.vue-flow__panel .fold-content>div,
+.vue-flow__panel .fold-content>div div,
+.vue-flow__panel .fold-content>div button,
+.vue-flow__panel .fold-content>div select,
+.vue-flow__panel .fold-content>div label {
   height: 24px;
+  width: 100%;
+  margin: 0;
+  padding-top: 0;
+  padding-bottom: 0;
+  line-height: 24px;
+}
+.vue-flow__panel .fold-content>div {
+  margin-bottom: 2px;
+}
+.vue-flow__panel .fold-content>div>label {
+  display:flex;
+  justify-content:space-between;
+  align-items:center;
+  gap:5px;
+  cursor:pointer;
 }
 </style>
