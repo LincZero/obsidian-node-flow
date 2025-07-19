a6072bee8eb5eff3b114207b96783419d003d3ca
Linc <762699299@qq.com>
2025-07-19 22:50:26 +0800

    ci: add EditableCodeblockApp workflow3

--- src/EditableCodeblockApp/src/components/TabBar.vue
+++ src/EditableCodeblockApp/src/components/TabBar.vue
@@ -25,7 +25,7 @@
       <div class="menus-2" v-show="clickedMenu==='serialLinks'">
         <div class="menu-2"><a href="https://any-block.github.io/any-block/">AnyBlock</a></div>
         <div class="menu-2"><a href="https://linczero.github.io/obsidian-node-flow/">NodeFlow</a></div>
-        <div class="menu-2"><a href="https://linczero.github.io/obsidian-node-flow/EditableCodeblock.html">EditableCodeblock</a></div>
+        <div class="menu-2"><a href="https://linczero.github.io/obsidian-node-flow/EditableCodeblock/">EditableCodeblock</a></div>
         <div class="menu-2"><a href="https://linczero.github.io/obsidian-chat-view-qq/">ChatView</a></div>
         <div></div>
       </div>
